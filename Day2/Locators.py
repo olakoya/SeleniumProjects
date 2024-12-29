1b8b2f12fa9ec9af243209b8adbd16d76e7348ca
olakoya <ola.koya1@gmail.com>
2024-12-29 18:29:10 +0000

    updates

---
+++ Day2/Locators.py
@@ -0,0 +1,44 @@
+'''
+Locators in Selenium Webdriver
+--------------------------------
+Every web applications have elements.
+And every element contains action that's performed
+And the element can be validated
+House address is what one calls location
+And the letter sent to the house address that is location is called an action
+We need to locate the element to perform action on it
+
+Locators are called addresses of web elements
+Every web elements has  a location
+We need locators because with the help of web element we can perform some actions
+
+In another word we need locators to identify web element on web page
+And to perform some action on that web element
+And to perform action on this web element we need a locator
+
+We have different types of locators
+------------------------------------
+1. Basic or Normal Locators or Locators
+    i. Id
+    ii. Name
+    iii. Linktext or Partial Linktext
+    iv. Class Name
+    v. Tage Name
+
+2. Customized Locators
+    a. Css Selector
+    b. XPath
+
+2a. CSS Selector
+----------------
+i. Tag and ID
+ii. Tag and Class
+iii. Tag and Attribute
+iv. Tag, Class and Attribute
+
+2b. XPath
+-----------
+i. Absolute XPath
+ii. Relative XPath
+
+'''
