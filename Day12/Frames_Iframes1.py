37e78e4671a5c9ebbfc62a7d7a0002706609c81e
olakoya <ola.koya1@gmail.com>
2025-01-21 21:31:04 +0000

    updates

--- Day12/Frames_Iframes1.py
+++ Day12/Frames_Iframes1.py
@@ -31,6 +31,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 opt = webdriver.ChromeOptions()
 opt.add_experimental_option("detach" , True)
+opt.add_argument("--incognito")
+opt.add_experimental_option("excludeSwitches", ["enable-automation"])
+opt.add_argument("--lang=hi")
 driver = webdriver.Chrome(options=opt)
 driver.implicitly_wait(10)
 driver.get("https://ui.vision/demo/webtest/frames/")
